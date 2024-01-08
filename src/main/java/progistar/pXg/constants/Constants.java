38ab1b74ef52e482ff1d9d100fd53cc93cccbed1
gistar <progistar@naver.com>
2024-01-08 14:47:15 -0600

    [tiny] add version

--- src/main/java/progistar/pXg/constants/Constants.java
+++ src/main/java/progistar/pXg/constants/Constants.java
@@ -7,7 +7,7 @@ public class Constants {
 	// minor: critical bug fix, change in results something like that.
 	// patch: tiny bug fix, typo, and change code styles something like that.
 	public static final String VERSION = "pXg v2.1.1";
-	public static final String RELEASE = "(release 2024-01-05)";
+	public static final String RELEASE = "(release 2024-01-08)";
 	public static final String INTRODUCE = "Seunghyuk Choi and Eunok Paek in the Department of Computer Science at Hanyang University in Seoul, South Korea. ";
 	
 	public static long	UNIQUE_RUN_ID	=	0;
