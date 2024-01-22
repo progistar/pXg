e0584d9a9ba67cf836a10e8252cb0a82aae6e701
gistar <progistar@naver.com>
2024-01-22 00:22:19 -0600

    [hotfix] fix frame error in softclip

--- src/main/java/progistar/pXg/constants/Constants.java
+++ src/main/java/progistar/pXg/constants/Constants.java
@@ -6,8 +6,8 @@ public class Constants {
 	//        plus, basically, this version number intends to upgrade behaviors of tool.
 	// minor: critical bug fix, change in results something like that.
 	// patch: tiny bug fix, typo, and change code styles something like that.
-	public static final String VERSION = "pXg v2.1.1";
-	public static final String RELEASE = "(release 2024-01-08)";
+	public static final String VERSION = "pXg v2.1.2";
+	public static final String RELEASE = "(release 2024-01-22)";
 	public static final String INTRODUCE = "Seunghyuk Choi and Eunok Paek in the Department of Computer Science at Hanyang University in Seoul, South Korea. ";
 	
 	public static long	UNIQUE_RUN_ID	=	0;
